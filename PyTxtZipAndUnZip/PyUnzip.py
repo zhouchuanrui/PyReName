0ff775de4d22e41118234024d1036a9f46cba1ec
zcrBA <zhouchuanrui@foxmail.com>
2016-01-28 10:19:38 +0800

    add argparse to pyunzip

--- PyTxtZipAndUnZip/PyUnzip.py
+++ PyTxtZipAndUnZip/PyUnzip.py
@@ -2,14 +2,16 @@
 import sys
 import os
 import pdb
+import argparse
 
 class OneToFiles(object):
     """docstring for OneToFiles"""
-    def __init__(self, arg):
+    def __init__(self, arg, default_dir = "exact/", key_file = "AllInOne.txt"):
         super(OneToFiles, self).__init__()
         self.arg = arg
         self.header = "zhimakaimen:"
-        self.default_dir = "exact/"
+        self.default_dir = default_dir
+        self.key_file = key_file
 
     def cdToDefaultDir(self):
         """docstring for cdToDefaultDir"""
@@ -19,7 +21,7 @@ class OneToFiles(object):
         print("Create ", self.default_dir)
         pass
 
-    def fileUnzip(self, key_file = "AllInOne.txt"):
+    def fileUnzip(self):
         """docstring for fileUnzip"""
         buf = ""
         fdr = open(key_file, "r")
@@ -46,7 +48,24 @@ class OneToFiles(object):
 
 def main():
     """docstring for main"""
-    otf = OneToFiles(0)
+    global warning_cnt
+
+    apsr = argparse.ArgumentParser()
+    apsr.add_argument("-s", "--source", help = "Indicate input source file.")
+    apsr.add_argument("-o", "--outputdir", help = "Indicate output directory.")
+    args = apsr.parse_args()
+
+    if args.source:
+        source_file = args.source
+    else:
+        source_file = "AllInOne.txt"
+
+    if args.outputdir:
+        output_dir = args.outputdir
+    else:
+        output_dir = "exact/"
+
+    otf = OneToFiles(0, output_dir, source_file)
     otf.fileUnzip()
     pass
 
